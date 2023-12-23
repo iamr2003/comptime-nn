a84f5669f3659e9d19cfdbf9e831e06bef01c851
iamr2003 <roughtar@gmail.com>
2023-12-22 20:06:33 -0500

    hacky solution to pass sizes through

--- src/main.zig
+++ src/main.zig
@@ -5,13 +5,12 @@ const std = @import("std");
 fn Layer(comptime input_nodes: usize, comptime output_nodes: usize, comptime in_activation: fn (in: f64) f64) type {
     const layerType = struct {
         weights: [output_nodes][input_nodes]f64 = [_][input_nodes]f64{[_]f64{1} ** input_nodes} ** output_nodes,
-        input_size: usize = input_nodes,
-        output_size: usize = output_nodes,
+        comptime input_size: usize = input_nodes,
+        comptime output_size: usize = output_nodes,
 
-        //have to pass weights to ourselves bc yes
         pub fn forward(weights: [output_nodes][input_nodes]f64, input: [input_nodes]f64) [output_nodes]f64 {
             var output: [output_nodes]f64 = [_]f64{0} ** output_nodes;
-            for ( 0..output_nodes) | output_index| {
+            for (0..output_nodes) |output_index| {
                 var sum: f64 = 0;
                 for (input, 0..) |input_val, input_index| {
                     sum += weights[output_index][input_index] * input_val;
@@ -26,14 +25,20 @@ fn Layer(comptime input_nodes: usize, comptime output_nodes: usize, comptime in_
 }
 
 fn Network(comptime input_layers: type) type {
-    //need to fix this to be an array
-    return struct {
-        layer: input_layers = input_layers{},
-        //all of this part is pretty annoying
-        // pub fn forward(){
-        //
-        // }
-    };
+    comptime {
+        //this is so dumb and hacky
+        const la: input_layers = input_layers{};
+        //need to fix this to be an array
+        const network_type = struct {
+            comptime layer: input_layers = input_layers{},
+            // hardcode dumbness, also annoyances of functions being from type
+            // and not member
+            pub fn forward(l: input_layers, in: [la.input_size]f64) [la.output_size]f64 {
+                return input_layers.forward(l.weights, in);
+            }
+        };
+        return network_type;
+    }
 }
 
 fn relu(in: f64) f64 {
@@ -54,13 +59,12 @@ test "layer test single var" {
     try std.testing.expectEqual(l1.forward(layer.weights, .{1}), .{1});
     try std.testing.expectEqual(l1.forward(layer.weights, .{5}), .{5});
     try std.testing.expectEqual(l1.forward(layer.weights, .{-1}), .{0});
-
 }
 
 test "nn test single var single layer" {
     const nn_type = Network(Layer(1, 1, relu));
     var nn: nn_type = nn_type{};
-    //function not preserved?
-    try std.testing.expectEqual(nn.layer.forward(nn.layer.weights, .{-1}), .{0});
-    try std.testing.expectEqual(nn.layer.forward(nn.layer.weights, .{1}), .{1});
+    // //function not preserved?
+    try std.testing.expectEqual(nn_type.forward(nn.layer, .{-1}), .{0});
+    try std.testing.expectEqual(nn_type.forward(nn.layer, .{1}), .{1});
 }
