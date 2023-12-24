2c79aa52f0e6c504fc1ad483590042305816df39
iamr2003 <roughtar@gmail.com>
2023-12-23 22:02:53 -0500

    got single variable test working with variable named

--- src/grad.zig
+++ src/grad.zig
@@ -1,9 +1,16 @@
 // pub const cosh = @import("complex/cosh.zig").sinh;
 // pub const tanh = @import("complex/tanh.zig").tanh;
+const std = @import("std");
 
 //may all become SIMD'd at some point
 //this is the traditional way to do it, basically same code as micrograd
-const GradVal = struct { val: f64, grad: f64 };
+const GradVal = struct {
+    val: f64,
+    grad: f64,
+    // with_respect_to: ?*const []u8 = null,
+};
+
+//need to store NAME of var,
 //still a lot of questions about multi dimensions with weights
 
 //give basic binary ops, building up in the traditional way
@@ -24,13 +31,19 @@ inline fn mul(lhs: GradVal, rhs: GradVal) GradVal {
 
 inline fn div(lhs: GradVal, rhs: GradVal) GradVal {
     return GradVal{
-        .val = lhs.val/rhs.val,
-        .grad = ((rhs.val*lhs.grad) - (lhs.val*rhs.grad))/(rhs.val * rhs.val),
+        .val = lhs.val / rhs.val,
+        .grad = ((rhs.val * lhs.grad) - (lhs.val * rhs.grad)) / (rhs.val * rhs.val),
     };
 }
 
-inline fn literal(lit: f64) GradVal {
-    return GradVal{ .val = lit, .grad = 0 };
+//goal is a lot of inefficiency compiles out
+//we are going to duplicate expression a million times, perhaps leaving tree intact would be more helpful
+inline fn variable(val: f64, name: []const u8, respect: []const u8) GradVal {
+    return GradVal{ .val = val, .grad = if (std.mem.eql(u8,name, respect)) 1 else 0 };
+}
+
+inline fn literal(val: f64) GradVal {
+    return GradVal{ .val = val, .grad = 0 };
 }
 
 inline fn max(lhs: GradVal, rhs: GradVal) GradVal {
@@ -65,9 +78,26 @@ inline fn exp(in: GradVal) GradVal {
 //tanh, ELU other common ones
 
 inline fn relu(in: GradVal) GradVal {
-    return max(literal(0), in);
+    return max(variable(0), in);
 }
 
 inline fn sigmoid(in: GradVal) GradVal {
-    return div(exp(in), add(literal(1), exp(in)));
+    return div(exp(in), add(variable(1), exp(in)));
+}
+
+//examples
+fn square(val: f64) GradVal {
+    return mul(variable(val, "x", "x"), variable(val, "x", "x"));
+}
+
+fn abs(val: f64) GradVal {
+    return max(variable(val, "x", "x"), mul(literal(-1), variable(val, "x", "x")));
+}
+
+test "simple single var" {
+    try std.testing.expectEqual(square(2).grad, (GradVal{ .val = 4, .grad = 4 }).grad);
+    try std.testing.expectEqual(square(5), GradVal{ .val = 25, .grad = 10 });
+
+    try std.testing.expectEqual(abs(7), GradVal{ .val = 7, .grad = 1 });
+    try std.testing.expectEqual(abs(-7), GradVal{ .val = 7, .grad = -1 });
 }
--- src/main.zig
+++ src/main.zig
@@ -2,6 +2,7 @@ const std = @import("std");
 
 //I might not need to be quite this silly
 //might just implement the autograd way and use zig comptime to make it happen
+//let's propogate through a single layer first
 fn Layer(comptime input_nodes: usize, comptime output_nodes: usize, comptime in_activation: fn (in: f64) f64) type {
     const layerType = struct {
         weights: [output_nodes][input_nodes]f64 = [_][input_nodes]f64{[_]f64{1} ** input_nodes} ** output_nodes,
@@ -24,26 +25,65 @@ fn Layer(comptime input_nodes: usize, comptime output_nodes: usize, comptime in_
     return layerType;
 }
 
-fn Network(comptime input_layers:[]const type) type {
-    comptime {
-        //initialize one of each type
-        
-        // //this is so dumb and hacky
-        const front:input_layers[0] = input_layers[0]{};
-        const back:input_layers[input_layers.len - 1 ] = input_layers[input_layers.len - 1]{};
-        //need to fix this to be an array
-        const network_type = struct {
-            //need to figure out 
-            layer: input_layers[0] = input_layers[0]{},
-            // // hardcode dumbness, also annoyances of functions being from type
-            // // and not member
-            pub fn forward(l: input_layers[0], in: [front.input_size]f64) [back.output_size]f64 {
-                return input_layers[0].forward(l.weights, in);
-            }
-        };
-        return network_type;
-    }
-}
+// const nonCompLayer = struct {
+//     //some annoying stuff with partial initialization
+//     weights: [][]f64,
+//     input_size: usize,
+//     output_size: usize,
+//     activation: fn (in: f64) f64,
+//
+//     pub fn init(comptime input: usize, comptime output: usize, comptime act: fn (in: f64) f64) nonCompLayer {
+//         return nonCompLayer{ .weights = [_][input]f64{[_]f64{1} ** input} ** output, .in_activation = act, .input_size = input, .output_size = output };
+//     }
+//
+//     pub fn forward(l: nonCompLayer, input: []f64) []f64 {
+//         var output: [l.input_size]f64 = [_]f64{0} ** l.output_size;
+//         for (0..l.output_size) |output_index| {
+//             var sum: f64 = 0;
+//             for (input, 0..) |input_val, input_index| {
+//                 sum += l.weights[output_index][input_index] * input_val;
+//             }
+//             sum /= l.input_size;
+//             output[output_index] = l.activation(sum);
+//         }
+//         return output;
+//     }
+// };
+//
+// const nonCompNetwork = struct {
+//     layers: []nonCompLayer,
+//
+//     pub fn forward(n: nonCompNetwork, input: []f64) []f64 {
+//         for (n.layers) |l| {
+//             input = l.forward(input);
+//         }
+//         return input;
+//     }
+// };
+//
+// fn Network(comptime input_layers: []const type) type {
+//     comptime {
+//         //initialize one of each type to get info
+//         const front: input_layers[0] = input_layers[0]{};
+//         const back: input_layers[input_layers.len - 1] = input_layers[input_layers.len - 1]{};
+//
+//         const network_type = struct {
+//             //need to figure out
+//             comptime layers: []const type = input_layers,
+//             //no I need to store values as well
+//             // // hardcode dumbness, also annoyances of functions being from type
+//             // // and not member
+//             pub fn forward(layers: []const type, in: [front.input_size]f64) [back.output_size]f64 {
+//                 //messiness should hopefully get cleaned up by compiler
+//                 //would like to do this recursively tbh, composition of functions
+//                 // need to compose a list of functions
+//
+//                 return layers[0].forward(front[0].weights, in);
+//             }
+//         };
+//         return network_type;
+//     }
+// }
 
 fn relu(in: f64) f64 {
     return @max(0, in);
@@ -65,14 +105,21 @@ test "layer test single var" {
     try std.testing.expectEqual(l1.forward(layer.weights, .{-1}), .{0});
 }
 
-test "nn test single var single layer" {
-    const layers: [1]type = .{Layer(1, 1, relu)};
-    const nn_type = Network(&layers);
-    var nn: nn_type = nn_type{};
-    // //function not preserved?
-    try std.testing.expectEqual(nn_type.forward(nn.layer, .{-1}), .{0});
-    try std.testing.expectEqual(nn_type.forward(nn.layer, .{1}), .{1});
-}
+// test "non comp layer" {
+//     const l: nonCompLayer = nonCompLayer.init(1, 1, relu);
+//     try std.testing.expectEqual(nonCompLayer.forward(l, .{1}), .{1});
+//     try std.testing.expectEqual(nonCompLayer.forward(l, .{-1}), .{0});
+//     try std.testing.expectEqual(nonCompLayer.forward(l, .{5}), .{5});
+// }
+
+// test "nn test single var single layer" {
+//     const layers: [1]type = .{Layer(1, 1, relu)};
+//     const nn_type = Network(&layers);
+//     var nn: nn_type = nn_type{};
+//     // //function not preserved?
+//     try std.testing.expectEqual(nn_type.forward(nn.layer, .{-1}), .{0});
+//     try std.testing.expectEqual(nn_type.forward(nn.layer, .{1}), .{1});
+// }
 
 // test "array of types" {
 //     comptime{
